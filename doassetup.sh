c19317b3a3d8ddb0c3d11c9dbd78ffca1e5af380
NebulaBC <contact@nebulabc.net>
2021-11-23 16:07:16 -0500

    Update doassetup.sh

--- doassetup.sh
+++ doassetup.sh
@@ -4,5 +4,6 @@ doas rm -rf /etc/lynx/README.md
 doas rm -rf /etc/lynx/setup.sh
 doas rm -rf /etc/lynx/CNAME
 doas rm -rf /etc/lynx/.git
+doas rm -rf /etc/lynx/doassetup.sh
 doas touch /etc/lynx/credits.md
 doas bash -c "echo .lynx config is created by NebulaBC - https://github.com/NebulaBC/.lynx > /etc/lynx/credits.md"
