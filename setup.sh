365fda0eac6c12f732831ecb09bebe5c42090307
NebulaBC <contact@nebulabc.net>
2021-11-23 15:46:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ rm -rf /etc/lynx/setup.sh
 rm -rf /etc/lynx/CNAME
 touch /etc/lynx/Credits.md
 echo ".lynx config is created by NebulaBC - https://github.com/NebulaBC/.lynx" > /etc/lynx/Credits.md
+exit
