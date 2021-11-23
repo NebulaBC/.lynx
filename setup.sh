d07a2c6f6ceb4089b40a1a94bfb7d5ac2bbafc74
NebulaBC <contact@nebulabc.net>
2021-11-23 15:51:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-sudo su
-rm -rf /etc/lynx
-git clone https://github.com/NebulaBC/.lynx /etc/lynx
-rm -rf /etc/lynx/README.md
-rm -rf /etc/lynx/setup.sh
-rm -rf /etc/lynx/CNAME
-touch /etc/lynx/Credits.md
-echo ".lynx config is created by NebulaBC - https://github.com/NebulaBC/.lynx" > /etc/lynx/Credits.md
-exit
+sudo rm -rf /etc/lynx
+sudo git clone https://github.com/NebulaBC/.lynx /etc/lynx
+sudo rm -rf /etc/lynx/README.md
+sudo rm -rf /etc/lynx/setup.sh
+sudo rm -rf /etc/lynx/CNAME
+sudo touch /etc/lynx/Credits.md
+sudo echo ".lynx config is created by NebulaBC - https://github.com/NebulaBC/.lynx" > /etc/lynx/Credits.md
